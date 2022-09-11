9a47e2d173aaa976aa914f18bfdc094f7f103ef9
Bohdan Lesyk <b0gdan41k2007@gmail.com>
2022-09-11 21:11:09 +0300

    Lesson 3. Class Assertions. `@Test` annotation

---
+++ src/main/java/inc/lesyk/bohdan/junit/service/UserService.java
@@ -0,0 +1,13 @@
+package inc.lesyk.bohdan.junit.service;
+
+import inc.lesyk.bohdan.junit.dto.User;
+
+import java.util.Collections;
+import java.util.List;
+
+public class UserService {
+
+    public List<User> getAllUsers() {
+        return Collections.emptyList();
+    }
+}
