7b4f267098ebd5258b5d2e37f2b12603eef09566
Bohdan Lesyk <b0gdan41k2007@gmail.com>
2022-09-19 09:43:40 +0300

    Lesson 4. `@BeforeAll`, `@BeforeEach`, `@AfterEach` & `@AfterAll`

--- src/main/java/inc/lesyk/bohdan/junit/service/UserService.java
+++ src/main/java/inc/lesyk/bohdan/junit/service/UserService.java
@@ -2,12 +2,17 @@ package inc.lesyk.bohdan.junit.service;
 
 import inc.lesyk.bohdan.junit.dto.User;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserService {
+    private final List<User> users = new ArrayList<>();
 
     public List<User> getAllUsers() {
-        return Collections.emptyList();
+        return users;
+    }
+
+    public boolean add(User user) {
+        return users.add(user);
     }
 }
